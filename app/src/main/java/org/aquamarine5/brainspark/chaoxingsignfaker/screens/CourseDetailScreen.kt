be7751a785a81a35395528bc06a42396c35738b5
aquamarine5 <3168287806@QQ.com>
2025-03-15 14:41:43 +0800

    zz

--- app/src/main/java/org/aquamarine5/brainspark/chaoxingsignfaker/screens/CourseDetailScreen.kt
+++ app/src/main/java/org/aquamarine5/brainspark/chaoxingsignfaker/screens/CourseDetailScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -22,6 +23,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import org.aquamarine5.brainspark.chaoxingsignfaker.R
@@ -54,15 +56,16 @@ fun CourseDetailScreen(
             if (activitiesData == null) {
                 CenterCircularProgressIndicator()
             } else {
-                Row(verticalAlignment = Alignment.CenterVertically) {
+                Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.fillMaxWidth()) {
                     Icon(painterResource(R.drawable.ic_arrow_left), contentDescription = null)
                     Spacer(modifier = Modifier.height(8.dp))
                     Text(
                         "课程名称：${courseEntity.courseName}",
                         color = Color.DarkGray,
+                        textAlign = TextAlign.Left,
                         modifier = Modifier.clickable {
                             navController.navigate(CourseListDestination)
-                        })
+                        }.fillMaxWidth())
 
                 }
                 Spacer(modifier = Modifier.height(12.dp))
